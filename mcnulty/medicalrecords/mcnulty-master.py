530da8cb47326e0654c35d0e48c74db9e4c482fa
michaelmoliterno <michaelmoliterno@Michaels-MacBook-Pro-3.local>
2015-02-10 12:54:29 -0500

    adding mcnulty-master

---
+++ mcnulty/medicalrecords/mcnulty-master.py
@@ -0,0 +1,21 @@
+import sqlite3
+import pandas as pd
+
+print 'hello world'
+
+
+con = sqlite3.connect('compData.db')
+
+
+test_patients = pd.read_sql("SELECT count(distinct(tp.patientguid)) from training_patient tp join training_diagnosis td \
+		where tp.patientguid = td.patientguid  and td.ICD9Code like '%314.00%'", con)
+
+training_patients = pd.read_sql("SELECT count(distinct(tp.patientguid)) from test_patient tp join test_diagnosis td \
+		where tp.patientguid = td.patientguid  and td.ICD9Code like '%314.00%'", con)
+
+print test_patients
+print training_patients
+
+
+
+
